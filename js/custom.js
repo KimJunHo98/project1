148109968658a8a63f7390b133bcf8c77d242784
KimJunHo98 <dydgnscc6845@gmail.com>
2022-12-29 21:36:45 +0900

    투뎁스 메뉴 자바스크립트 적용 예정

---
+++ js/custom.js
@@ -0,0 +1,3 @@
+const gnb = document.querySelector(".gnb");
+const sm = document.querySelectorAll(".header .nav .gnb > li > ul.sub_menu > li");
+const smBg = document.querySelector("#header > .sub_menu_bg");
