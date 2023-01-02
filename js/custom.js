3147cb67d56bf69ea910f7837f277943433576f8
KimJunHo98 <dydgnscc6845@gmail.com>
2023-01-03 00:12:48 +0900

    메인 비주얼 jquery 슬라이드 적용, css 수정

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,42 @@
-// gnb에 마우스 오버할 때
-let nav = document.querySelector(".nav"),
-    // subMenu = document.querySelectorAll(".sub_menu"),
-    subBg = document.querySelector(".sub_menu_bg");
-
-nav.addEventListener("mouseover", function(){
-    subBg.classList.add("show");
-});
-nav.addEventListener("mouseleave", function(){
-    subBg.classList.remove("show");
-});
-
-// 슬라이드 배너
-let slideWrap = document.getElementsByClassName(".main_visual"),
-    slide = document.getElementsByClassName("main_visual_box"),
-    slideCnt = slide.length,
-    current = 0,
-    btnPrev = document.getElementsByClassName("prev"),
-    btnnext = document.getElementsByClassName("next");
-// javascript
-
-// -------------- jQuery ------------------
-// gnb에 마우스 오버할 때
-let subMenu = $(".sub_menu");
-
 $(function(){
+    // gnb에 마우스 오버할 때
+    let nav = $(".nav"),
+        subMenu = $(".sub_menu"),
+        subBg = $(".sub_menu_bg");
+
+    $(nav).on("mouseover", function(){
+        $(subBg).addClass("show");
+    });
+    $(nav).on("mouseleave", function(){
+        $(subBg).removeClass("show");
+    });
+    
+    // 서브메뉴
     $(nav).on("mouseover", function(){
         $(subMenu).addClass("show");
     });
     $(nav).on("mouseleave", function(){
         $(subMenu).removeClass("show");
     });
+
+    // 슬라이드 메인 비주얼
+    let slides = $(".main_visual_box"),
+        slideCnt = slides.length, // 슬라이드 개수
+        btnPrev = $(".main_visual_btn .prev"),
+        btnNext = $(".main_visual_btn .next"),
+        current = 0;
+        
+    slides.eq(current).fadeIn();
+
+    // 자동 슬라이드
+    setInterval(showNextSlide, 5000); 
+    function showNextSlide(){
+        let next = (current+1)%slideCnt;
+        // 현재 슬라이드 사라지고 다음 슬라이드 보여줌
+        slides.eq(current).fadeOut();
+        slides.eq(next).fadeIn();
+        current = next;
+    };
 });
+
 
