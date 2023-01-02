89cfcf143511c77a5baac218bbc9053190f94812
KimJunHo98 <dydgnscc6845@gmail.com>
2023-01-02 10:24:46 +0900

    gnb 스크립트 적용, 메인 비주얼 수정

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,32 @@
-const gnb = document.querySelector(".gnb");
-const sm = document.querySelectorAll(".header .nav .gnb > li > ul.sub_menu > li");
-const smBg = document.querySelector("#header > .sub_menu_bg");
+// gnb에 마우스 오버할 때
+let nav = document.querySelector("nav"),
+    subBg = document.querySelector(".sub_menu_bg");
+
+nav.addEventListener("mouseover", function(){
+    subBg.classList.add("show");
+});
+nav.addEventListener("mouseleave", function(){
+    subBg.classList.remove("show");
+});
+
+
+// javascript
+
+
+// -------------- jQuery ------------------
+// gnb에 마우스 오버할 때
+let subMenu = $(".sub_menu");
+
+$(function(){
+    $(nav).on("mouseover", function(){
+        $(subMenu).addClass("show");
+    });
+    $(nav).on("mouseleave", function(){
+        $(subMenu).removeClass("show");
+    });
+});
+
+// 슬라이드 배너
+$(function(){
+    
+});
